2ffd843957cbdf60b0c0eebb3aabbd600fb9ab00
unknown <gupta353@purdue.edu>
2020-07-13 15:39:18 +0100

    nodDataValue added in computation of CI_prod

--- snap/MERIS_processing/compute_composite_CI.py
+++ snap/MERIS_processing/compute_composite_CI.py
@@ -24,7 +24,8 @@ def computeCompositeCI(compositeProduct):
     newBandName = 'composite_CI_0'
     datatype = 'float32'
     expression = CI_bandnames[0]
-    CI_prod=gpfOP.BandMathsAG(compositeProduct,newBandName,datatype,expression)
+    noDataVal='nan'
+    CI_prod=gpfOP.BandMathsAG(compositeProduct,newBandName,datatype,expression,noDataVal)
 
     # merge
     geographicError='NaN'
@@ -35,7 +36,7 @@ def computeCompositeCI(compositeProduct):
         newBandName = 'composite_CI_'+str(comp_ind+1)
         datatype = 'float32'
         expression = 'if (not nan(composite_CI_'+str(comp_ind)+') and not nan('+CI_bandnames[comp_ind]+')) then max(composite_CI_'+str(comp_ind)+','+CI_bandnames[comp_ind]+')'+' else (if not nan(composite_CI_'+str(comp_ind)+') then composite_CI_'+str(comp_ind)+' else '+CI_bandnames[comp_ind]+')'
-        CI_prod=gpfOP.BandMathsAG(compProduct,newBandName,datatype,expression)
+        CI_prod=gpfOP.BandMathsAG(compProduct,newBandName,datatype,expression,noDataVal)
 
         # Merge
         compProduct=gpfOP.MergeAG(compProduct,CI_prod,geographicError)
