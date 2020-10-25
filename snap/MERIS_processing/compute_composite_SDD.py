fe8c9f23e938bf73b3d99e62199b5f3d021a25ff
unknown <gupta353@purdue.edu>
2020-10-25 19:36:10 +0100

    the variable 'expression' changed

--- snap/MERIS_processing/compute_composite_SDD.py
+++ snap/MERIS_processing/compute_composite_SDD.py
@@ -18,10 +18,10 @@ def computeCompositeSDD(compositeProduct):
     
     # read all the CI bands
     bandnames=list(compositeProduct.getBandNames())
-    SDD_bandnames=[var for var in bandnames if var.split('_')[0]=='secchi' and len(var.split('_'))>2 ]
+    SDD_bandnames=[var for var in bandnames if var.split('_')[0]=='secchi' and len(var.split('_'))>5 ]
 
     # Apply bandmaths in for loop
-    newBandName = 'composite_secchi_depth_0'
+    newBandName = 'composite_secchi_depth_over_pos_CI_0'
     datatype = 'float32'
     expression = SDD_bandnames[0]
     noDataVal='nan'
@@ -34,11 +34,11 @@ def computeCompositeSDD(compositeProduct):
     for comp_ind in range(0,len(SDD_bandnames)):
         # Apply BandMath
         if comp_ind == len(SDD_bandnames)-1:
-            newBandName = 'composite_secchi_depth_final'
+            newBandName = 'composite_secchi_depth_over_pos_CI_final'
         else:
-            newBandName = 'composite_secchi_depth_'+str(comp_ind+1)
+            newBandName = 'composite_secchi_depth_over_pos_CI_'+str(comp_ind+1)
         datatype = 'float32'
-        expression = 'if (not nan(composite_secchi_depth_'+str(comp_ind)+') and not nan('+SDD_bandnames[comp_ind]+')) then max(composite_secchi_depth_'+str(comp_ind)+','+SDD_bandnames[comp_ind]+')'+' else (if not nan(composite_secchi_depth_'+str(comp_ind)+') then composite_secchi_depth_'+str(comp_ind)+' else '+SDD_bandnames[comp_ind]+')'
+        expression = 'if (not nan(composite_secchi_depth_over_pos_CI_'+str(comp_ind)+') and not nan('+SDD_bandnames[comp_ind]+')) then max(composite_secchi_depth_over_pos_CI_'+str(comp_ind)+','+SDD_bandnames[comp_ind]+')'+' else (if not nan(composite_secchi_depth_over_pos_CI_'+str(comp_ind)+') then composite_secchi_depth_over_pos_CI_'+str(comp_ind)+' else '+SDD_bandnames[comp_ind]+')'
         SDD_prod=gpfOP.BandMathsAG(compProduct,newBandName,datatype,expression,noDataVal)
 
         # Merge
