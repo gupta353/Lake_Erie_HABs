7b0c813dbb2523ecf7385e9528986784e5461e0b
unknown <gupta353@purdue.edu>
2020-10-25 19:35:12 +0100

    expression for secchi depth changed to compute it only over positive CI pixels

--- snap/MERIS_processing/secchi_depth_computation.py
+++ snap/MERIS_processing/secchi_depth_computation.py
@@ -49,9 +49,9 @@ def sd_compute(direc, fname):
         return
 
     # Use BandMaths operator to compute secchi depth
-    newBandName = 'secchi_depth'
+    newBandName = 'secchi_depth_over_pos_CI'
     datatype = 'float32'
-    expression = '(LE_Mask == 1 and water) and (reflec_6>0 and reflec_4>0)? exp10(-1.04*(reflec_6/reflec_4) + 0.99): NaN'
+    expression = '((LE_Mask == 1 and water) and (reflec_6>0 and reflec_4>0) and CI>0)? exp10(-1.04*(reflec_6/reflec_4) + 0.99): NaN'
     noDataVal='nan'
     sd_prod=gpfOP.BandMathsAG(product,newBandName,datatype,expression,noDataVal)
 
