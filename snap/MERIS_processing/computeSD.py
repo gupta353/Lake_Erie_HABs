c9ee27a7702051af455f5676ea63133510444eec
unknown <gupta353@purdue.edu>
2020-10-25 19:37:42 +0100

    only directory path was changed

--- snap/MERIS_processing/computeSD.py
+++ snap/MERIS_processing/computeSD.py
@@ -16,7 +16,7 @@ import os
 from snappy import ProductIO
 
 #path
-direc='D:/Research/EPA_Project/Lake_Erie_HAB/Data/remote_sensing_data/gupta353_MERIS_full_resolution_L2_2008_001_2020-05-20T01-12-24'
+direc='D:/Research/EPA_Project/Lake_Erie_HAB/Data/remote_sensing_data/gupta353_MERIS_full_resolution_L2_2011_001_2020-05-21T00-51-46'
 
 # read product
 # list all the products with extension '.dim'
