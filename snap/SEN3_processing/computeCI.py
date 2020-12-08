5ee2021872088940bb3700730430fa1dd30f2ad8
unknown <gupta353@purdue.edu>
2020-12-08 17:33:02 +0000

    code to read the products changed

--- snap/SEN3_processing/computeCI.py
+++ snap/SEN3_processing/computeCI.py
@@ -12,19 +12,19 @@ sys.path.append('C:\\Users\\Administrator\\.snap\\snap-python') # or sys.path.in
 import snappy
 import CI_computation
 import os
+import shutil
 
 from snappy import ProductIO
 
 #path
-direc='E:/EPA/lake_erie_HAB/remote_sensing_data/MERIS_RR/2011'
+direc = 'D:/Research/EPA_Project/Lake_Erie_HAB/Data/remote_sensing_data/Sentinel/2016'
+
 
 # read product
-# list all the products with extension N1
-
-fname_list=os.listdir(direc)
+# list all the products with extension
+fname_list = os.listdir(direc)
 for fname in fname_list:
-    direc1 = direc+'/'+fname
-    if os.path.isdir(direc1):
-        fname1 = os.listdir(direc1)
-        CI_computation.CI_compute(direc1,fname1[0])
+    if os.path.splitext(fname)[1] == '.SEN3':
+        CI_computation.CI_compute(direc,fname)
+
         
