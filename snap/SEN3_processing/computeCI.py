9f375d774954d249783bfda4512b609d38df744c
unknown <gupta353@purdue.edu>
2020-12-15 03:19:28 +0000

    no chnage

--- snap/SEN3_processing/computeCI.py
+++ snap/SEN3_processing/computeCI.py
@@ -23,6 +23,7 @@ direc = 'D:/Research/EPA_Project/Lake_Erie_HAB/Data/remote_sensing_data/Sentinel
 # read product
 # list all the products with extension
 fname_list = os.listdir(direc)
+#fname_list = ['S3A_OL_2_WFR_20160822T152431_104.SEN3']
 for fname in fname_list:
     if os.path.splitext(fname)[1] == '.SEN3':
         CI_computation.CI_compute(direc,fname)
