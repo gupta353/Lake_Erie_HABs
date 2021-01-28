8446487a327d9cd156709045332ff0a7cc04166d
unknown <gupta353@purdue.edu>
2021-01-28 21:35:35 +0000

    code for mosaicking opeartion added

--- snap/SEN3_processing/composite_SDD.py
+++ snap/SEN3_processing/composite_SDD.py
@@ -18,6 +18,7 @@ import datetime
 import math
 import compute_composite_CI
 import re
+import gpfOP
 
 from snappy import ProductIO
 from snappy import jpy
@@ -31,7 +32,7 @@ WKTReader = jpy.get_type('com.vividsolutions.jts.io.WKTReader')
 NodeDescriptor = jpy.get_type('org.esa.snap.core.gpf.common.MergeOp$NodeDescriptor')
 
 #path
-direc='D:/Research/EPA_Project/Lake_Erie_HAB/Data/remote_sensing_data/gupta353_MERIS_full_resolution_L2_2011_001_2020-05-21T00-51-46'
+direc='D:/Research/EPA_Project/Lake_Erie_HAB/Data/remote_sensing_data/Sentinel/2020'
 # target widht and target heights of resamplig
 targetWidth=500
 targetHeight=300
@@ -46,8 +47,8 @@ fid.close()
 time_window_per=10
 
 # being and end dates
-begin_date='2011-05-01'
-end_date='2011-10-31'
+begin_date='2020-05-01'
+end_date='2020-10-31'
 
 # list the min-max dates of each time-window including begin_date
 begin_date_obj=datetime.datetime.strptime(begin_date,'%Y-%m-%d')
@@ -67,19 +68,110 @@ fname_list=os.listdir(direc)
 for prod in filter_prods:
     fname_list.remove(prod)    
 
+# remove all the products with extension other than '.dim'
+fname_list = [fname for fname in fname_list if fname.endswith('.dim')]
+
 # list the dates of each product
 product_date=[]
 product_datenum=[]
 product_engdate=[]
 for fname in fname_list:
     if fname.endswith("dim"):
-        x=fname.split('_')[2]
-        y=datetime.datetime.strptime(x.split('2PPBCM')[1],'%Y%m%d')
+        x=fname.split('_')[4]
+        y=datetime.datetime.strptime(x.split('T')[0],'%Y%m%d')
         z=date.toordinal(y)
         product_datenum.append(z)
         product_date.append([fname,z])
-        product_engdate.append(x.split('2PPBCM')[1])
-        
+        #product_engdate.append(x.split('2PPBCM')[1])
+
+# Create a product containing sd band (filled with) with maximum size out of the products to be listed in the same time-window
+for tw_ind in range(0,num_time_windows):
+    ind=[i for i, datenum_value in enumerate(product_datenum) if(datenum_value>=time_window[tw_ind][0] and datenum_value<=time_window[tw_ind][1])] # find the indices of the product that are contained in this time-window
+
+    if len(ind) != 0:
+        widths = []
+        heights = []
+        fname_tw = []
+        for i in ind:
+            fname = fname_list[i]
+            fname_tw.append(fname)
+            filename = direc+'/'+fname
+            product = ProductIO.readProduct(filename)
+            CI = product.getBand('secchi_depth_over_pos_CI')
+            widths.append(CI.getRasterWidth())
+            heights.append(CI.getRasterHeight())
+
+            max_width = max(widths)
+            max_height = max(heights)
+
+            i = [i for i in range(len(widths)) if widths[i]==max_width]
+
+            # create a NaN band in the product with largest width
+            fname = fname_tw[i[0]]
+            filename = direc+'/'+fname
+            product = ProductIO.readProduct(filename)
+            newBandName = 'secchi_depth_over_pos_CI'
+            datatype = 'float32'
+            expression = 'NaN'
+            noDataVal='nan'
+            composite_prod = gpfOP.BandMathsAG(product,newBandName,datatype,expression,noDataVal);
+
+            # create a Lake_erie_water band
+            newBandName = 'secchi_depth_over_pos_CI'
+            datatype = 'float32'
+            expression = 'WQSF_lsb_INLAND_WATER?1:0'
+            noDataVal='nan'
+            prod_tmp = gpfOP.BandMathsAG(product,newBandName,datatype,expression,noDataVal);
+
+            #composite_prod = gpfOP.MergeAG(composite_prod,prod_tmp,'NaN')
+            
+        ## mosaic composite_prod with other products in the time-window
+        #  mosaic parameters
+        westBound = -83.60220985614447
+        eastBound = -81.78879308692983
+        northBound = 42.31086488661365
+        southBound = 41.1893175594222
+        pixelSizeY = 0.00270
+        pixelSizeX = 0.00357
+
+        # mosaic water band
+        varName = 'LE_mask'
+        varExp = 'secchi_depth_over_pos_CI'
+        product1 = ProductIO.readProduct(filename)
+        LE_mask_prd = gpfOP.mosaicAG(composite_prod,prod_tmp,westBound,eastBound,northBound,southBound,pixelSizeX,pixelSizeY,varName,varExp)
+
+        count = 0
+        for fname in fname_tw:
+            count = count + 1
+            filename = direc+'/'+fname
+            varName = 'secchi_depth_over_pos_CI_'+str(count)
+            varExp = 'secchi_depth_over_pos_CI'
+            product1 = ProductIO.readProduct(filename)
+            prd_tmp = gpfOP.mosaicAG(composite_prod,product1,westBound,eastBound,northBound,southBound,pixelSizeX,pixelSizeY,varName,varExp)
+
+            newBandName = 'secchi_depth_over_pos_CI_'+str(count)
+            datatype = 'float32'
+            expression = 'secchi_depth_over_pos_CI_'+str(count)+'_count ==1?secchi_depth_over_pos_CI_'+str(count)+':NaN'
+            noDataVal = 'NaN'
+            prd_tmp = gpfOP.BandMathsAG(prd_tmp,newBandName,datatype,expression,noDataVal)
+
+            composite_prod = gpfOP.MergeAG(composite_prod,prd_tmp,'NaN')
+
+        # compute composite image
+        composite_prod = gpfOP.MergeAG(composite_prod,LE_mask_prd,'NaN')
+        compositeProduct=computeCompositeSDD(composite_prod)
+                
+        # write the composite product
+        bdate_obj=datetime.date.fromordinal(time_window[tw_ind][0])
+        bdate=str(bdate_obj.year)+str(bdate_obj.month)+str(bdate_obj.day)
+        edate_obj=datetime.date.fromordinal(time_window[tw_ind][1])
+        edate=str(edate_obj.year)+str(edate_obj.month)+str(edate_obj.day)
+
+        wfname='S3A_OL_2_WFR_'+bdate+'_'+edate+'.dim'
+        wfilename=direc+'/composite_sd_product/'+wfname
+        ProductIO.writeProduct(compositeProduct,wfilename,'BEAM-DIMAP')
+
+"""
 # resample, merge products and compute composite image in each time-window
 for tw_ind in range(0,num_time_windows):
     ind=[i for i, datenum_value in enumerate(product_datenum) if(datenum_value>=time_window[tw_ind][0] and datenum_value<=time_window[tw_ind][1])] # find the indices of the product that are contained in this time-window
@@ -135,3 +227,4 @@ for tw_ind in range(0,num_time_windows):
         wfname='MER_FRS_2PPBCM_'+bdate+'_'+edate+'.dim'
         wfilename=direc+'/composite_sd_product_1/'+wfname
         ProductIO.writeProduct(compositeProduct,wfilename,'BEAM-DIMAP')
+"""
