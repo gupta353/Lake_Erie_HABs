73d1f3f3d444a428ffcf65af1e33dc1276d06027
unknown <gupta353@purdue.edu>
2021-01-28 21:48:58 +0000

    several minor changes to run the script for new data

--- data_collection_and_processing/combine_met_data.m
+++ data_collection_and_processing/combine_met_data.m
@@ -5,10 +5,11 @@ clear all
 close all
 clc
 
-direc = 'D:/Research/EPA_Project/Lake_Erie_HAB/Data/lake_erie_NOAA/meteorological_data/erie-cmt_bydate';
+direc = 'D:/Research/EPA_Project/Lake_Erie_HAB/Data/lake_erie_NOAA/meteorological_data/erie-cmt_bydate/fine_timescale_data';
 
-fnames = {'wdir_avg_2004.txt','wdir_avg_2005.txt','wdir_avg_2006.txt','wdir_avg_2007.txt','wdir_avg_2008.txt','wdir_avg_2009.txt','wdir_avg_2010.txt',...
-    'wdir_avg_2011.txt','wdir_avg_2012.txt','wdir_avg_2013.txt','wdir_avg_2014.txt','wdir_avg_2015.txt'};
+fnames = {'atemp_2004.txt','atemp_2005.txt','atemp_2006.txt','atemp_2007.txt','atemp_2008.txt','atemp_2009.txt','atemp_2010.txt',...
+    'atemp_2011.txt','atemp_2012.txt','atemp_2013.txt','atemp_2014.txt','atemp_2015.txt','atemp_2016.txt',...
+    'atemp_2017.txt','atemp_2018.txt','atemp_2019.txt','atemp_2020.txt'};
 
 final_data=[];
 for fname_ind = 1:length(fnames)
@@ -23,10 +24,10 @@ for fname_ind = 1:length(fnames)
 end
 
 % write data to a textfile
-fname = 'wdir.txt';
+fname = 'atemp.txt';
 filename = fullfile(direc,fname);
 fid = fopen(filename,'w');
-fprintf(fid,'%s\t%s\t%s\t%s\t%s\t%s\t%s\n','year','month','day','hour','minute','sec','wind_direction(Deg)');
+fprintf(fid,'%s\t%s\t%s\t%s\t%s\t%s\t%s\n','year','month','day','hour','minute','sec','atemp(DegC)');
 for wind = 1:size(final_data,1)
     
     fprintf(fid,'%d\t%d\t%d\t%d\t%d\t%d\t%f\n',final_data(wind,:));
