96f82977ac7fd225f0d12dc6a4eb67135f28a763
unknown <gupta353@purdue.edu>
2021-01-28 21:32:54 +0000

    the value of variable 'direc' and 'fname_list(commented)'changed

--- snap/SEN3_processing/computeCI.py
+++ snap/SEN3_processing/computeCI.py
@@ -17,13 +17,13 @@ import shutil
 from snappy import ProductIO
 
 #path
-direc = 'D:/Research/EPA_Project/Lake_Erie_HAB/Data/remote_sensing_data/Sentinel/2016'
+direc = 'D:/Research/EPA_Project/Lake_Erie_HAB/Data/remote_sensing_data/Sentinel/2020'
 
 
 # read product
 # list all the products with extension
 fname_list = os.listdir(direc)
-#fname_list = ['S3A_OL_2_WFR_20160822T152431_104.SEN3']
+#fname_list = ['S3A_OL_2_WFR_20160505T155126_6.SEN3']
 for fname in fname_list:
     if os.path.splitext(fname)[1] == '.SEN3':
         CI_computation.CI_compute(direc,fname)
