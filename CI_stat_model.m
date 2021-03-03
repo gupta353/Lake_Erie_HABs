73200124032bbfa5a1d156d3547cca6e0e6c123a
unknown <gupta353@purdue.edu>
2021-03-03 23:07:01 +0000

    script to implement random forest in bootstrap sampling scheme

--- CI_stat_model.m
+++ CI_stat_model.m
@@ -61,9 +61,9 @@ preds = preds./repmat(preds_std,size(preds,1),1);
 % preds(:,end) = [];
 
 % calibration and validation data
-%{
+%
 rng(1);
-count = 0;
+
 for count = 1:1000
     
     cal_ind = randsample(length(CI),107);
@@ -79,48 +79,68 @@ for count = 1:1000
     % CI_cal = log(CI(cal_ind)); preds_cal = preds(cal_ind,:);
     % CI_val = log(CI(val_ind)); preds_val = preds(val_ind,:);
     %% random forest
-    %{
-% cross-validation
-NumTrees=50:80;
-NVarToSample=2:15;          % number of predictors that random forest considers at each node
-MinLeaf=1:5;
-count = 0;
-
-for par1_ind = 1:length(NumTrees)
-    for par2_ind = 1:length(NVarToSample)
-        for par3_ind = 1:length(MinLeaf)
-            count = count+1;
-            
-            %   predfun = @(Xcal,ycal,Xval)RFreg(Xcal,ycal,Xval,NumTrees(par1_ind),NVarToSample(par2_ind),MinLeaf(par3_ind));
-            %   err = crossval('mse',preds_cal,CI_cal,'predfun',predfun);
-            %   err_mat(count,:) = [NumTrees(par1_ind),NVarToSample(par2_ind),MinLeaf(par3_ind),err];
-            B = TreeBagger(NumTrees(par1_ind),preds_cal,CI_cal,...
-                'Method','regression','NVarToSample',NVarToSample(par2_ind),'MinLeaf',MinLeaf(par3_ind));
-            Fitted_val=predict(B,preds_val);
-            scatter(CI_val,Fitted_val); hold on
-            ylim([0 10])
-            xlim([0 10])
-            plot([0 10],[0 10],'color','black')
-            R2(count,:)=[NumTrees(par1_ind),NVarToSample(par2_ind),MinLeaf(par3_ind),corr(CI_val,Fitted_val)^2];
-%             title(['R^2 = ',num2str(R2(count,4))])
-%             pause;
-%             close all
+    %
+    % cross-validation
+    NumTrees=25:25:100;
+    NVarToSample=4:4:16;          % number of predictors that random forest considers at each node
+    MinLeaf=2:2:6;
+    
+    rcount = 0;
+    for par1_ind = 1:length(NumTrees)
+        for par2_ind = 1:length(NVarToSample)
+            for par3_ind = 1:length(MinLeaf)
+                rcount = rcount+1;
+                
+                %   predfun = @(Xcal,ycal,Xval)RFreg(Xcal,ycal,Xval,NumTrees(par1_ind),NVarToSample(par2_ind),MinLeaf(par3_ind));
+                %   err = crossval('mse',preds_cal,CI_cal,'predfun',predfun);
+                %   err_mat(count,:) = [NumTrees(par1_ind),NVarToSample(par2_ind),MinLeaf(par3_ind),err];
+%                 B = TreeBagger(NumTrees(par1_ind),preds_cal,CI_cal,...
+%                     'Method','regression','NVarToSample',NVarToSample(par2_ind),'MinLeaf',MinLeaf(par3_ind));
+%                 Fitted_val=predict(B,preds_val);
+%                 scatter(CI_val,Fitted_val); hold on
+%                 ylim([0 10])
+%                 xlim([0 10])
+%                 plot([0 10],[0 10],'color','black')
+%                 R2(rcount,:)=[NumTrees(par1_ind),NVarToSample(par2_ind),MinLeaf(par3_ind),corr(CI_val,Fitted_val)^2];
+                %             title(['R^2 = ',num2str(R2(count,4))])
+                %             pause;
+                %             close all
+                predfun = @(Xtrain,ytrain,Xtest)RFag(Xtrain,ytrain,Xtest,NumTrees(par1_ind),NVarToSample(par2_ind),MinLeaf(par3_ind));
+                val = crossval('mse',preds_cal,CI_cal,'predfun',predfun);
+                mse(rcount,:) = [NumTrees(par1_ind),NVarToSample(par2_ind),MinLeaf(par3_ind),val];
+            end
         end
     end
-end
-
-ind = find(R2(:,4) == max(R2(:,4)));
-B = TreeBagger(R2(ind,1),preds_cal,CI_cal,...
-    'Method','regression','NVarToSample',R2(ind,2),'MinLeaf',R2(ind,3));
-Fitted_val=predict(B,preds_val);
-scatter(CI_val,Fitted_val)
-ylim([0 700])
-xlim([0 700])
-R21=corr(CI_val,Fitted_val)^2;
-title(['R^2 = ',num2str(R21)])
+    
+    ind = find(mse(:,4) == min(mse(:,4)));
+    B = TreeBagger(mse(ind,1),preds_cal,CI_cal,...
+        'Method','regression','NVarToSample',mse(ind,2),'MinLeaf',mse(ind,3),'oobvarimp','on');
+    Fitted_val=predict(B,preds_val);
+    importance(count,:) = B.OOBPermutedVarDeltaError;
+    scatter(CI_val,Fitted_val,'filled'); hold on
+    ylim([2 8])
+    xlim([2 8])
+    plot([0 10],[0 10],'color','black','linewidth',2); hold off
+    
+    R21(count) = corr(CI_val,Fitted_val)^2;
+    R21(count) = round(R21(count)*100)/100;
+    title(['R^2 = ',num2str(R21(count))])
+    
+    xlabel('Observed log(CI)','fontname','arial','fontsize',12)
+    ylabel('Predicted log(CI)','fontname','arial','fontsize',12)
+    box('on')
+    box.linewidth = 2;
+    set(gca,'fontname','arial','fontsize',12,box)
+    
+    % save figure
+    fname = strcat('obs_pred_log_CI_',num2str(count),'.jpg');
+    filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots/RF_regression_plots_1',fname);
+    print(filename,'-r300','-djpeg')
+    clear box
+    
     %}
     
-    % linear regression
+    %% linear regression
     %{
 mdl=fitlm(preds_cal,CI_cal);
 Fitted_val = predict(mdl,preds_val);
@@ -133,8 +153,8 @@ plot([0 10],[0 10],'color','black')
 R21=corr(CI_val,Fitted_val)^2;
     %}
     
-    % lasso regression
-    %
+    %% lasso regression
+    %{
     [B,Fitinfo] = lasso(preds_cal,CI_cal,'alpha',0.999,'CV',5);
     ind = find(Fitinfo.MSE == min(Fitinfo.MSE));
     beta(:,count) = [Fitinfo.Intercept(ind);B(:,ind)];
@@ -162,7 +182,7 @@ R21=corr(CI_val,Fitted_val)^2;
     filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots/lasso_regression_plots_1',fname);
     print(filename,'-r300','-djpeg')
     close all
-    
+    %}
 end
 %
 hist(R21)
@@ -172,29 +192,29 @@ fname = strcat('R2_histogram.svg');
 filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots/lasso_regression_plots_1',fname);
 fig2svg(filename);
 %
-hist(lambda)
-xlabel('Regularization parameter (\lambda)','fontname','arial','fontsize',12);
-ylabel('Number of samples in the bin','fontname','arial','fontsize',12)
-fname = strcat('lambda_histogram.svg');
-filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots/lasso_regression_plots_1',fname);
-fig2svg(filename);
-%
+% hist(lambda)
+% xlabel('Regularization parameter (\lambda)','fontname','arial','fontsize',12);
+% ylabel('Number of samples in the bin','fontname','arial','fontsize',12)
+% fname = strcat('lambda_histogram.svg');
+% filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots/RF_regression_plots_1',fname);
+% fig2svg(filename);
+%{
 for pind = 1:length(pred_name)
     
     hist(beta(1+pind,:))
     xlabel(num2str(pind),'fontname','arial','fontsize',12);
 end
 
-boxplot(beta(2:end,:)',1:38);
+boxplot(importance,1:38);
 ylabel('Regression coefficients','fontname','arial','fontsize',12)
 set(gca,'fontname','arial','fontsize',10,'plotboxaspectratio',[2 1 1])
 fname = 'coefficient_distribtuion.svg';
-filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots/lasso_regression_plots_1',fname);
+filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots/RF_regression_plots_1',fname);
 fig2svg(filename);
 %}
 %}
 %% cross-validation (no validation) lasso
-%
+%{
 for CI_ind =  1:length(CI)
     
     val_ind = CI_ind;
@@ -229,7 +249,7 @@ filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots',f
 fig2svg(filename)
 %}
 % Uncertainty analysis of residuals
-%
+%{
 res = CI - Fitted_val;
 std_dev = std(res);
 upper_bound = Fitted_val + 2*std_dev;
