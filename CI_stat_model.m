3c83ac4e60882222d380e888bd983b3f5df3c97e
Abhinav Gupta <Abhinav.Gupta@dri.edu>
2021-08-30 20:14:15 -0700

    LASSO model for 20 day lead time prediction, corr-lag and time-step variables not included

--- CI_stat_model.m
+++ CI_stat_model.m
@@ -5,7 +5,7 @@ close all
 clc
 
 % read data
-fname = 'model_data_10_corr_CI_lag_included_and_TP_TKN_previous_120_and_time_step_included.txt';
+fname = 'model_data_20_corr_CI_lag_included_and_TP_TKN_previous_120_and_time_step_included.txt';
 filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB','matlab_codes',fname);
 fid = fopen(filename,'r');
 formatspec = ['%s',repmat('%f',1,82)];
@@ -66,7 +66,7 @@ preds = preds./repmat(preds_std,size(preds,1),1);
 % preds(:,end) = [];
 
 % calibration and validation data
-%
+%{
 rng(1);
 
 for count = 1:1000
@@ -226,10 +226,10 @@ for count = 1:1000
     pause;
     clear box
     
-
-%}
 end
-%
+%}
+
+%{
 hist(R21)
 xlabel('Coefficient of determination (R^2)','fontname','arial','fontsize',12);
 ylabel('Number of samples in the bin','fontname','arial','fontsize',12)
@@ -267,7 +267,7 @@ saveas(gcf,filename,'svg');
 %}
 %}
 %% cross-validation (no validation)
-%{
+%
 for CI_ind =  1:length(CI)
     CI_ind
     val_ind = CI_ind;
@@ -276,7 +276,7 @@ for CI_ind =  1:length(CI)
     CI_val = CI(val_ind); preds_val = preds(val_ind,:);
     
     %% LASSO
-%{
+%
     [B,Fitinfo] = lasso(preds_cal,CI_cal,'alpha',0.999,'CV',5);
     ind = find(Fitinfo.MSE == min(Fitinfo.MSE));
     beta = [Fitinfo.Intercept(ind);B(:,ind)];
@@ -284,7 +284,7 @@ for CI_ind =  1:length(CI)
 %}
     
     %% Random Forest
-%
+%{
      NumTrees=25:25:100;
     NVarToSample=4:4:16;          % number of predictors that random forest considers at each node
     MinLeaf=2:2:6;
@@ -307,8 +307,9 @@ for CI_ind =  1:length(CI)
     B = TreeBagger(mse(ind,1),preds_cal,CI_cal,...
         'Method','regression','NVarToSample',mse(ind,2),'MinLeaf',mse(ind,3),'oobvarimp','on');
     Fitted_val(CI_ind,1)=predict(B,preds_val);
-end
 
+%}
+end
 
 scatter(CI,Fitted_val,'filled'); hold on
 ylim([2 6.5])
@@ -326,12 +327,12 @@ title(['R^2 = ',num2str(R2)],'fontname','arial','fontsize',12);
 clear box
 
 % save plot
-fname = 'RF_obs_pred_log_CI_cross_validation_cc10_removed_corr_lag_vars_and_time_steps_predictor_added.svg';
+fname = 'lasso_obs_pred_log_CI_cross_validation_cc10_removed.svg';
 filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots_08_28_2021',fname);
 saveas(gcf,filename,'svg')
 
 % save data
-fname = 'RF_obs_pred_log_CI_cross_validation_cc10_removed_corr_lag_vars_and_time_steps_predictor_added.mat';
+fname = 'lasso_obs_pred_log_CI_cross_validation_cc10_removed.mat';
 filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots_08_28_2021',fname);
 save(filename);
 %}
