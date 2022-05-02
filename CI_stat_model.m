59d89294dd5d7fad3f5dc09407f80cef88cce2a5
Abhinav Gupta <Abhinav.Gupta@dri.edu>
2022-05-02 16:46:30 -0700

    BS scheme: LASSO model for 30 days lead time prediction with multi-time lagged variables: data on validation mse is also saved

--- CI_stat_model.m
+++ CI_stat_model.m
@@ -101,7 +101,7 @@ parfor count = 1:1000
     % CI_cal = log(CI(cal_ind)); preds_cal = preds(cal_ind,:);
     % CI_val = log(CI(val_ind)); preds_val = preds(val_ind,:);
     %% random forest
-%
+%{
     % cross-validation
     NumTrees = 100;
     NVarToSample = 4:4:16;          % number of predictors that random forest considers at each node
@@ -165,7 +165,7 @@ parfor count = 1:1000
 %}
     
     %% lasso regression
-%{
+%
     [B,Fitinfo] = lasso(preds_cal,CI_cal,'alpha',0.999,'CV',5);
     ind = find(Fitinfo.MSE == min(Fitinfo.MSE));
     beta(:,count) = [Fitinfo.Intercept(ind);B(:,ind)];
@@ -190,7 +190,7 @@ parfor count = 1:1000
     set(gca,'fontname','arial','fontsize',12)
 
     fname = strcat('obs_pred_log_CI_',num2str(count),'.jpg');
-    filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots_04_28_2022/LASSO_regression_plots_BS',fname);
+    filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots_04_28_2022/LASSO_regression_plots_BS_corr_lag_vars_added',fname);
     print(filename,'-r300','-djpeg')
     close all
 %}
@@ -271,7 +271,7 @@ hist(R21)
 xlabel('Coefficient of determination (R^2)','fontname','arial','fontsize',12);
 ylabel('Number of samples in the bin','fontname','arial','fontsize',12)
 fname = strcat('R2_histogram.svg');
-filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots_04_28_2022/RF_regression_plots_BS_corr_lag_vars_added',fname);
+filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots_04_28_2022/LASSO_regression_plots_BS_corr_lag_vars_added',fname);
 saveas(gcf,filename,'svg');
 %
 % hist(lambda)
@@ -288,24 +288,24 @@ saveas(gcf,filename,'svg');
 %     xlabel(num2str(pind),'fontname','arial','fontsize',12);
 % end
 
-% beta_plot = beta(2:end,:);
-% boxplot(beta_plot',1:55);
-% ylabel('Regression coefficients','fontname','arial','fontsize',12)
-% set(gca,'fontname','arial','fontsize',10,'plotboxaspectratio',[2 1 1])
-% fname = 'coefficient_distribtuion.svg';
-% filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots_04_28_2022/LASSO_regression_plots_BS',fname);
-% saveas(gcf,filename,'svg');
+beta_plot = beta(2:end,:);
+boxplot(beta_plot',1:55);
+ylabel('Regression coefficients','fontname','arial','fontsize',12)
+set(gca,'fontname','arial','fontsize',10,'plotboxaspectratio',[2 1 1])
+fname = 'coefficient_distribtuion.svg';
+filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots_04_28_2022/LASSO_regression_plots_BS_corr_lag_vars_added',fname);
+saveas(gcf,filename,'svg');
 
  
-% boxplot(importance,1:55);
+% boxplot(importance,1:72);
 % ylabel('Predictor importance','fontname','arial','fontsize',12)
 % set(gca,'fontname','arial','fontsize',10,'plotboxaspectratio',[2 1 1])
 % fname = 'predictor_importance.svg';
-% filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots_04_28_2022/RF_regression_plots_BS',fname);
+% filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots_04_28_2022/RF_regression_plots_BS_corr_lag_vars_added',fname);
 % saveas(gcf,filename,'svg');
 %}
 % save data
-fname = 'RF_BS_data_corr_lag_vars_added.mat';
+fname = 'LASSO_BS_data_corr_lag_vars_added.mat';
 filename = fullfile('D:/Research/EPA_Project/Lake_Erie_HAB/matlab_codes/plots_04_28_2022',fname);
 save(filename)
 %}
